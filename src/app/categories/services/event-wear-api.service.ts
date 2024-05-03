ae4b2fe4bf99793a59d71933bfbac8481174f490
TonyRamosNajar <102090663+TonyRamosNajar@users.noreply.github.com>
2024-05-03 08:45:19 -0500

    Add files via upload

---
+++ src/app/categories/services/event-wear-api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventWearApiService {
+  private baseUrl = 'https://my-json-server.typicode.com/SI729-2401-WS51-Grupo-4/db-server'
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/Category`);
+  }
+}
+/*
+http://localhost:3000*/
